32f6cd911a9e5708942c6078293b244d5907a9e9
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-10 01:20:12 +0100

    display pool champ

--- src/app/components/champion/champion.component.ts
+++ src/app/components/champion/champion.component.ts
@@ -1,12 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { Champions } from '../../interfaces/champions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-champion',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './champion.component.html',
   styleUrl: './champion.component.css',
 })
 export class ChampionComponent {
   @Input() champion!: Champions;
+
+  url_image = 'https://ddragon.leagueoflegends.com/cdn/15.3.1/img/champion/';
+
+  selectChampion() {
+    this.champion.selected = !this.champion.selected;
+  }
 }
