a9dde944514df9160c02ec9c212aebfe89c295ea
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-11 02:41:00 +0100

    add champ filter WIP

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,13 +1,15 @@
 import { Component, inject, Signal, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ChampionsService } from '../../services/champions.service';
 import { Champions } from '../../interfaces/champions';
 import { ChampionComponent } from '../champion/champion.component';
 import { CommonModule } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { SearchPipe } from '../../pipes/search.pipe';
 
 @Component({
   selector: 'app-board',
-  imports: [CommonModule, ChampionComponent],
+  imports: [CommonModule, ChampionComponent, FormsModule, SearchPipe],
   templateUrl: './board.component.html',
   styleUrl: './board.component.css',
 })
@@ -18,7 +20,17 @@ export class BoardComponent {
   //variables
   champions: Signal<Champions[] | undefined> = signal<Champions[]>([]);
 
+  filterChampions: Signal<Champions[] | undefined> = signal<Champions[]>([]);
+
+  guessChampion: Champions | undefined;
+  guessChampionName: string = '';
+
   constructor() {
     this.champions = toSignal(this.championsService.getChampPool());
+    this.filterChampions = this.champions;
+  }
+
+  selectChampion(champion: Champions) {
+    this.guessChampion = champion;
   }
 }
