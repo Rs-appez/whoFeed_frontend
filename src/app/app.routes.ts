0e6b469fe90ae5b091bb182fef357ea1d59210e7
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-11 22:57:15 +0100

    add connect component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { BoardComponent } from './components/board/board.component';
+import { ConnectComponent } from './components/connect/connect.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: ConnectComponent,
+  },
   {
     path: 'board',
     component: BoardComponent,
