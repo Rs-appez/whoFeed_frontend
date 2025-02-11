44a592bccff28ee92040e2c33000cb51a4565b99
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-11 03:45:16 +0100

    remoce log

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -5,7 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
   transform(value: any[], keys: string, term: string): any[] {
-    console.log(value, keys, term);
     if (!term) return value;
     return value.filter((item) =>
       keys
