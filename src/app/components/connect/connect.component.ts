7286cd509c9533afddbaf0f799f6ef3e32f1e820
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-12 02:16:42 +0100

    make simple connect

--- src/app/components/connect/connect.component.ts
+++ src/app/components/connect/connect.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PlayerService } from '../../services/player.service';
+import { Player } from '../../player';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-connect',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './connect.component.html',
-  styleUrl: './connect.component.css'
+  styleUrl: './connect.component.css',
 })
 export class ConnectComponent {
+  // services
+  playerService: PlayerService = inject(PlayerService);
 
+  // variables
+  player: Player | undefined;
+  playerName: string = '';
+
+  createPlayer() {
+    if (this.playerName !== '') {
+      this.playerService.makePlayer(this.playerName).subscribe((player) => {
+        this.player = player;
+        this.savePlayer();
+      });
+    }
+  }
+
+  savePlayer() {
+    localStorage.setItem('player', JSON.stringify(this.player));
+  }
 }
