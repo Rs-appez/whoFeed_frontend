7909271372081397b35d30436e467725b7e99e45
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-12 02:16:24 +0100

    add player service

---
+++ src/app/services/player.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Player } from '../player';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlayerService {
+  http = inject(HttpClient);
+
+  private apiKey = environment.apiKey;
+  private backendUrl = environment.backendUrl;
+
+  url = `${this.backendUrl}/game/api/players`;
+
+  headers = new HttpHeaders({
+    Authorization: `TOKEN ${this.apiKey}`,
+    'Content-Type': 'application/json',
+  });
+
+  makePlayer(username: string): Observable<Player> {
+    let player = { name: username };
+    return this.http.post<Player>(`${this.url}/`, player, {
+      headers: this.headers,
+    });
+  }
+}
