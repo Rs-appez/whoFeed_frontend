bd86d78191a00d3744745700bb83257a15376029
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-16 18:29:41 +0100

    rework localStorageService

--- src/app/services/localstorage.service.ts
+++ src/app/services/localstorage.service.ts
@@ -1,12 +1,44 @@
-import { Injectable, inject } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID, inject, signal } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { PlayerService } from './player.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocalstorageService {
-  private readonly localStorage = inject(DOCUMENT)?.defaultView?.localStorage;
+  private readonly localStorage: Storage | undefined = undefined;
+  private isBrowser: boolean;
 
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(DOCUMENT) private document: Document,
+    private playerService: PlayerService,
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+    if (this.isBrowser) {
+      this.localStorage = this.document.defaultView?.localStorage;
+      console.log('localstorage service init');
+      window.addEventListener('storage', this.storageEventListener.bind(this));
+    }
+  }
+
+  //private listenForStorageChanges() {
+  //  console.log('listening for storage changes');
+  //  this.renderer.listen('window', 'storage', (event: StorageEvent) => {
+  //    console.log('storage event', event);
+  //    if (event.key === 'player') {
+  //      this.playerService.player = signal(this.get('player'));
+  //    }
+  //  });
+  //  console.log('listening for storage changes end of constructor');
+  //}
+
+  private storageEventListener(event: StorageEvent) {
+    console.log('storage event', event);
+    if (event.key === 'player') {
+      this.playerService.player = signal(this.get('player'));
+    }
+  }
   get<T>(key: string): T | null {
     const item = this.localStorage?.getItem(key);
 
@@ -18,11 +50,31 @@ export class LocalstorageService {
   }
 
   set(key: string, value: unknown): void {
+    if (!this.isBrowser) return;
+    const oldValue = this.localStorage?.getItem(key);
     this.localStorage?.setItem(key, JSON.stringify(value));
+    const event = new StorageEvent('storage', {
+      key: key,
+      newValue: JSON.stringify(value),
+      oldValue: oldValue,
+      storageArea: this.localStorage,
+      url: window.location.href,
+    });
+    window.dispatchEvent(event);
   }
 
   remove(key: string): void {
+    if (!this.isBrowser) return;
+    const oldValue = this.localStorage?.getItem(key);
     this.localStorage?.removeItem(key);
+    const event = new StorageEvent('storage', {
+      key: key,
+      newValue: null,
+      oldValue: oldValue,
+      storageArea: this.localStorage,
+      url: window.location.href,
+    });
+    window.dispatchEvent(event);
   }
 
   removeKeys(keys: string[]): void {
@@ -30,7 +82,16 @@ export class LocalstorageService {
   }
 
   clear(): void {
+    if (!this.isBrowser) return;
     this.localStorage?.clear();
+    const event = new StorageEvent('storage', {
+      key: null,
+      newValue: null,
+      oldValue: null,
+      storageArea: this.localStorage,
+      url: window.location.href,
+    });
+    window.dispatchEvent(event);
   }
 
   private isJSONValid(value: string): boolean {
