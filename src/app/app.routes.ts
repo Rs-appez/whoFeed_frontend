ceb1a405f7aeee0c86325f159f1e210ab8626c6c
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-16 18:30:22 +0100

    add route to select party

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { BoardComponent } from './components/board/board.component';
 import { ConnectComponent } from './components/connect/connect.component';
+import { SelectPartyComponent } from './components/select-party/select-party.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ConnectComponent,
+  },
+  {
+    path: 'party',
+    component: SelectPartyComponent,
   },
   {
     path: 'board',
