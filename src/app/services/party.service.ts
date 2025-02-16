4e6b5563722688116aeb9afcff73971a84896faf
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-16 19:33:49 +0100

    use WritableSignal

--- src/app/services/party.service.ts
+++ src/app/services/party.service.ts
@@ -1,5 +1,12 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { inject, Injectable, Signal } from '@angular/core';
+import {
+  effect,
+  inject,
+  Injectable,
+  signal,
+  Signal,
+  WritableSignal,
+} from '@angular/core';
 import { Player } from '../interfaces/player';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
@@ -10,7 +17,22 @@ import { PlayerService } from './player.service';
   providedIn: 'root',
 })
 export class PartyService {
+  constructor() {
+    effect(() => {
+      console.log('party service effect');
+      const currentPlayer = this.player();
+      console.log('currentPlayer', currentPlayer);
+      if (currentPlayer) {
+        this.headers = this.headers.set(
+          'Authorization',
+          `Bearer ${currentPlayer.jwttoken}`,
+        );
+      }
+    });
+  }
   http = inject(HttpClient);
+
+  party: WritableSignal<Party | null> = signal<Party | null>(null);
 
   private backendUrl = environment.backendUrl;
 
@@ -20,7 +42,6 @@ export class PartyService {
   private player: Signal<Player | null> = this.playerService.player;
 
   headers = new HttpHeaders({
-    Authorization: `Bearer ${this.player()!.jwttoken}`,
     'Content-Type': 'application/json',
   });
 
--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { inject, Injectable, signal, Signal } from '@angular/core';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Player } from '../interfaces/player';
 import { Observable } from 'rxjs/internal/Observable';
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs/internal/Observable';
 export class PlayerService {
   http = inject(HttpClient);
 
-  player: Signal<Player | null> = signal<Player | null>(null);
+  player: WritableSignal<Player | null> = signal<Player | null>(null);
 
   private backendUrl = environment.backendUrl;
 
