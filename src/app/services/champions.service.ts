7137aa1daec743315f24312d67fb64ae99a6859c
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-16 18:55:58 +0100

    refactor champion service

--- src/app/services/champions.service.ts
+++ src/app/services/champions.service.ts
@@ -1,10 +1,10 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, Signal } from '@angular/core';
 import { Champion } from '../interfaces/champions';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
-import { LocalstorageService } from './localstorage.service';
 import { Player } from '../interfaces/player';
+import { PlayerService } from './player.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,23 +12,19 @@ import { Player } from '../interfaces/player';
 export class ChampionsService {
   http = inject(HttpClient);
 
-  private localstorageService = inject(LocalstorageService);
   private backendUrl = environment.backendUrl;
+  private playerService = inject(PlayerService);
 
   url = `${this.backendUrl}/game/api/champions`;
 
-  private player: Player = this.localstorageService.get<Player>('player')!;
-  private jwtToken: string = '';
+  private player: Signal<Player | null> = this.playerService.player;
 
   headers = new HttpHeaders({
+    Authorization: `Bearer ${this.player()!.jwttoken}`,
     'Content-Type': 'application/json',
   });
 
   getChampPool(): Observable<Champion[]> {
-    this.headers = this.headers.set(
-      'Authorization',
-      `Bearer ${this.player.jwttoken}`,
-    );
     return this.http.get<Champion[]>(`${this.url}/get_champions/`, {
       headers: this.headers,
     });
